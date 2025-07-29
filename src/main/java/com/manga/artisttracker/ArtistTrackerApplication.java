bb367de613d9f3b6273d24711c1e920c0569ddb4
manavchaudhary1 <manavchaudhary041@gmail.com>
2025-07-29 23:00:57 +0530

    add PreDbRestoreInitializer to application startup for database restoration

--- src/main/java/com/manga/artisttracker/ArtistTrackerApplication.java
+++ src/main/java/com/manga/artisttracker/ArtistTrackerApplication.java
@@ -1,5 +1,6 @@
 package com.manga.artisttracker;
 
+import com.manga.artisttracker.config.PreDbRestoreInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ArtistTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArtistTrackerApplication.class, args);
+        SpringApplication app =  new SpringApplication(ArtistTrackerApplication.class);
+        app.addInitializers(new PreDbRestoreInitializer());
+        app.run(args);
     }
 
 }
