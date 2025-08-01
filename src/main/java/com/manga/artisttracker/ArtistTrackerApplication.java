b1d75612aa0684846d809ffffe6da76abca7e181
manavchaudhary1 <manavchaudhary041@gmail.com>
2025-08-01 23:26:11 +0530

    add logging for application startup in ArtistTrackerApplication

--- src/main/java/com/manga/artisttracker/ArtistTrackerApplication.java
+++ src/main/java/com/manga/artisttracker/ArtistTrackerApplication.java
@@ -1,18 +1,22 @@
 package com.manga.artisttracker;
 
 import com.manga.artisttracker.config.PreDbRestoreInitializer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class ArtistTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication app =  new SpringApplication(ArtistTrackerApplication.class);
         app.addInitializers(new PreDbRestoreInitializer());
         app.run(args);
+        log.info("ArtistTrackerApplication started");
+        log.info("Visit http://localhost:8080/dashboard ");
     }
 
 }
